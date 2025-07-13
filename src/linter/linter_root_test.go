708b1412cf6afa81f97fb214b50829889ffa1062
Luis Vasquez <luislve1796@gmail.com>
2025-07-13 11:43:28 -0500

    feat: Add banner info to root node

--- src/linter/linter_root_test.go
+++ src/linter/linter_root_test.go
@@ -28,3 +28,22 @@ func TestRunsLinterCreatingStructureGraph(t *testing.T) {
 	assert.Equal(LintStatusOK, result.Status)
 	assert.Equal("success", result.Msg)
 }
+
+func TestRunsLinterLoadingRootData(t *testing.T) {
+	assert := assert.New(t)
+
+	var manifest ManifestContent = ManifestContent(manifestWithRootDetails)
+	result, err := LintFromRoot(manifest, true)
+
+	assert.Nil(err)
+	assert.Equal(LintStatusOK, result.Status)
+	assert.Equal("success", result.Msg)
+
+	// Root
+	root := result.Structure.Root
+	assert.Equal("root", root.Info["type"])
+	assert.Equal("root", root.Info["id"])
+	assert.Equal(0, len(root.Links))
+	assert.Equal("https://raw.githubusercontent.com/luislve17/amauta/refs/heads/main/assets/amauta-logo.svg", root.Info["logoUrl"])
+	assert.Equal("https://github.com/luislve17/amauta", root.Info["githubUrl"])
+}
